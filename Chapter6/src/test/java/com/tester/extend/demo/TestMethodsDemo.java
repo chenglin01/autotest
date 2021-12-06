832c2f1e2fe51acdfdad5706e4ca7ec992cfcf32
chenglin01 <18513572604@163.com>
2021-12-06 23:06:51 +0800

    extentReport测试报告

---
+++ Chapter6/src/test/java/com/tester/extend/demo/TestMethodsDemo.java
@@ -0,0 +1,32 @@
+package com.tester.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+        throw new RuntimeException("这是失败的");
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+        Reporter.log("这是成功的");
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void teslogDemo(){
+        Reporter.log("这是自己的日志");
+        throw new RuntimeException("这是我运行的异常");
+    }
+
+}
